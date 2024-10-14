61bc4045dea08e728fa1f4eee211727674824fb0
boracingoz26 <boracingoz@gmail.com>
2024-10-14 12:28:22 +0300

    Restored accidentally deleted blobio game.

---
+++ Assets/Scripts/Slimes/Player/Player.cs
@@ -0,0 +1,52 @@
+﻿using Events;
+using Slimes.Pathfinding;
+using UnityEngine;
+using WorldObjects;
+
+namespace Slimes.Player
+{
+    public class Player : Slime
+    {
+        [SerializeField] private PlayerPathSetter _playerPathSetter;
+
+        protected override void IncreaseSize(int size)
+        {
+            base.IncreaseSize(size);
+            PlayerEvents.SizeIncreased?.Invoke(size);
+        }
+
+        protected override void Pause()
+        {
+            _playerPathSetter.Pause();
+        }
+
+        protected override void UnPause()
+        {
+            _playerPathSetter.UnPause();
+        }
+
+        public override void Eaten()
+        {
+            //base.Eaten();
+            
+        }
+
+        protected override void RegisterEvents()
+        {
+            base.RegisterEvents();
+            SlimeEvents.BaitCollision += OnBaitCollision;
+        }
+
+        protected override void OnBaitCollision(Bait colBait)
+        {
+            base.OnBaitCollision(colBait);
+            PlayerEvents.PlayerBaitConsume?.Invoke(Score);
+        }
+        
+        protected override void UnRegisterEvents()
+        {
+            base.UnRegisterEvents();
+            SlimeEvents.BaitCollision -= OnBaitCollision;
+        }
+    }
+}
